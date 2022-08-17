afc08091e996dab4e4fac365ba1bf38337db2de7
whitney <whitney.njogu@student.moringaschool.com>
2022-08-17 22:02:32 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=2
+let num2=31
+const multiply= (num1 * num2)
 
-// Write your code here
+const random=Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3=14
+let num4=10 
+const mod= (num3%num4)
+
+const max=Math.max(0,10,20)
